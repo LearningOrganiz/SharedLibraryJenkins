25cfc7841c85d7c30cd173d278157cc962f3def0
sriAjaygit <126223204+sriAjaygit@users.noreply.github.com>
2024-07-23 12:41:24 +0530

    Create Manipulations.groovy

---
+++ vars/Manipulations.groovy
@@ -0,0 +1,11 @@
+def sum(a,b){
+print "addation of ${a} and ${b} is ${a+b}"
+}
+
+def multplication(q,w){
+print "multiplication of ${q} and ${w} is ${q*w}"
+}
+
+def sub(x,y){
+print "subtraction of ${x} and ${y} is ${x-y}"
+}
